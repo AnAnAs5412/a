01fd85ad929823fb53eac6db6d4de5f8f6b77cf1
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-09-25 21:48:15 +0500

    ф
    ф

---
+++ 10 в 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число в десятичной системе счисления: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string b = Convert.ToString(a, 2);
+            Console.Write("Число в двоичной системе счисления: " + b);
+            Console.ReadKey();
+        }
+    }
+}
