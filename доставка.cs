57719c33ad46a4a86ee7b44fa6288b01b607f6aa
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-09-25 23:39:20 +0500

    ф
    ф

---
+++ доставка.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int t = Convert.ToInt32(Console.ReadLine());
+            if (n < 24 && n >= 0)
+            {
+                if (m < 60 && n >= 0)
+                {
+                    if (t < Math.Pow(10, 9) && t >= 30)
+                    {
+                        int c = n + t / 60;
+                        if (c >= 24)
+                        {
+                            c = c % 24;
+
+                        }
+                        int d = m + t % 60;
+                        if (d >= 60)
+                        {
+                            d = d - 60;
+                            c = c + 1;
+                        }
+                        if (d < 10)
+                        {
+                            Console.WriteLine($"Продукты привезут в {c}:0{d}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Продукты привезут в {c}:{d}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("Ошибка");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
