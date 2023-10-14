589c7b95f64ba81242f1fbb20677ebb284ee14c2
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-10-14 11:47:33 +0500

    число до 1000
    число до 1000

---
+++ число до 1000.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число до 1000: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a <= 1000)
+            {
+                for (int i = 0; i <= a; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введено число больше 1000");
+            }
+            Console.ReadKey();
+        }
+    }
+}
