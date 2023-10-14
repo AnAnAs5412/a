473bfb6d5cb655e1a2460b83b864f6c8250a620e
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-10-14 12:03:16 +0500

    ф
    ф

---
+++ массив в обратном.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            for (int i = 0; i < a.Length; i++) 
+            {
+                Console.Write("Введите число: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
