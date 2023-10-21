b007c4c0dad6618a0744fff0d7b7273450c40cd9
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-10-21 08:17:41 +0500

    ф
    ф

---
+++ Фибоначчи.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            Console.WriteLine(b);
+            int c = 1;
+            Console.WriteLine(c);
+            int e = a - 2;
+            while (e > 0)
+            {
+                int d = b + c;
+                Console.WriteLine(d);
+                b = c;
+                c = d;
+                e--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
