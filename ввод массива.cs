58c15d0dec7b4f8b6d6359302c51ee7bbe3684b0
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-10-22 11:13:03 +0500

    ввод массива
    ввод массива

---
+++ ввод массива.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] b = new int[a];
+            for (int i = 0; i < b.Length; i++) 
+            {
+                Console.Write($"Элемент массива {i}: ");
+                b[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Ваш массив: ");
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
