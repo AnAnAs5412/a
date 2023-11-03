9ebfb008a49754ba0ccbbbfb479b5bdca87545c0
AnAnAs5412 <145955899+AnAnAs5412@users.noreply.github.com>
2023-11-03 17:48:01 +0500

    разбитие строки по символу
    разбитие строки по символу

---
+++ разбитие строки по символу.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace разбитие_строки_по_символам
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите символ: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите строку: ");
+            string b = Console.ReadLine();
+            b = b.Replace(a, " ");
+            Console.Write("Строка: " + b);
+            Console.ReadKey();
+        }
+    }
+}
